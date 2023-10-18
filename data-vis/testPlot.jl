40552d5f93e85264d7dd6de735e94567c8ee7071
somwakdikar <somw@utexas.edu>
2023-10-17 20:57:08 -0500

    data-vis

---
+++ data-vis/testPlot.jl
@@ -0,0 +1,141 @@
+using Pkg
+
+# Pkg.add("CSV")
+# Pkg.add("DataFrames")
+# Pkg.add("Plots")
+# Pkg.add("CairoMakie")
+
+using CSV
+using DataFrames
+#using Plots
+# using Makie
+#using GLMakie
+using CairoMakie
+
+
+function get_data()
+    df = CSV.File("scatterplot.csv") |> DataFrame
+    # println(df)
+    x = df.X
+    y = df.Y
+    z = df.Z
+    d = df.D
+
+    # plt = scatter(x, y, z, color = d, marker_z = d, legend = false)
+    # display(plt)
+    # println(d)
+    return x, y, z, d
+end
+
+function generate_plot()
+    x, y, z, d = get_data()
+    d = d / 100.0
+
+
+    colors = ifelse.(d .< 50, :orange, :blue)
+    color_scale = cgrad([RGB(0.8, 0.8, 1), RGB(0, 0, 0.5)], d)
+
+
+    plt3d= Plots.plot(x, y, z,seriestype=:scatter, color = color_scale, marker_z = d, markersize = 3, title="Electron Positions", xlabel="X", ylabel="Y", zlabel="Z")
+    #plt3d = Plots.scatter(x, y, z, color = d, marker_z = d, colorbar = (title = "Color"), c = color_scale)
+    
+    
+
+    display(plt3d)
+
+end
+
+function generate_plot_makie(margin)
+    x, y, z, d = get_data()
+
+
+    zs = LinRange(0, 3, length(x))
+
+
+    fig = Figure()
+    ax = Axis3(
+        fig[1, 1], 
+        aspect = (1, 1, 1), 
+        title = "Electron Density Plot",
+        limits = (minimum(x)-margin, maximum(x)+margin, minimum(y)-margin, maximum(y)+margin, minimum(z)-margin, maximum(z)+margin),
+        xlabel = "X",
+        ylabel = "Y",
+        zlabel = "Z",
+
+        )
+    Colorbar(fig[1, 2], colormap = :viridis, flipaxis = false) 
+    
+    CairoMakie.scatter!(
+        ax, 
+        x, 
+        y, 
+        z, 
+        color = d,
+        markersize = d/5,
+        colormap = :viridis, 
+        )
+    display(fig)
+
+    # x = cos.(1:0.5:20)
+    # y = sin.(1:0.5:20)
+    # z = LinRange(0, 3, length(x))
+
+    #  s = CairoMakie.meshscatter(
+    #     x, 
+    #     y, 
+    #     z, 
+    #     markersize = 0.1,
+    #     color = d,
+
+    # )
+    # s
+    #limits = (minimum(x)-margin, maximum(x)+margin, minimum(y)-margin, maximum(y)+margin, minimum(z)-margin, maximum(z)+margin))
+
+end
+
+
+# generate_plot()
+
+generate_plot_makie(5)
+
+
+
+# y = rand(100)
+# plot(seriestype=:scattery, zcolor = abs.(y .- 0.5), m = (:heat, 0.8, Plots.stroke(1, :green)), ms = 10 * abs.(y .- 0.5) .+ 4, lab = "grad")
+# scatter!(y, zcolor = abs.(y .- 0.5), m = (:heat, 0.8, Plots.stroke(1, :green)), ms = 10 * abs.(y .- 0.5) .+ 4, lab = "grad")
+
+
+
+
+
+
+# default(legend = false)
+# x = y = range(-5, 5, length = 40)
+# zs = zeros(0, 40)
+# n = 100
+
+# @gif for i in range(0, stop = 2π, length = n)
+#     f(x, y) = sin(x + 10sin(i)) + cos(y)
+
+#     # create a plot with 3 subplots and a custom layout
+#     l = @layout [a{0.7w} b; c{0.2h}]
+#     p = plot(x, y, f, st = [:surface, :contourf], layout = l)
+
+#     # induce a slight oscillating camera angle sweep, in degrees (azimuth, altitude)
+#     plot!(p[1], camera = (10 * (1 + cos(i)), 40))
+
+#     # add a tracking line
+#     fixed_x = zeros(40)
+#     z = map(f, fixed_x, y)
+#     plot!(p[1], fixed_x, y, z, line = (:black, 5, 0.2))
+#     vline!(p[2], [0], line = (:black, 5))
+
+#     # add to and show the tracked values over time
+#     global zs = vcat(zs, z')
+#     plot!(p[3], zs, alpha = 0.2, palette = cgrad(:blues).colors)
+# end
+
+
+
+
+
