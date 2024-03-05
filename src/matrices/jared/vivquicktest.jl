9eec965b7ce06504d9380e6d191a282087412d40
Vivian Rogers <vivian.rogers@utexas.edu>
2024-03-05 00:34:32 -0600

    Added LowRankMatrices

--- src/matrices/jared/vivquicktest.jl
+++ src/matrices/jared/vivquicktest.jl
@@ -1,13 +1,17 @@
 using LinearAlgebra
 using SparseArrays
 include("LRA_type.jl")
+using .LRA_mod
 
 n = 512
 H = ComplexF64.(Tridiagonal(-ones(n-1),2*ones(n),-ones(n-1))) #+ 0.00001*im*I(n)
 #display(eigvals(H))
 
 #H = rand(n,n)
-H = LRA_mod.LRA(sparse(H),0.4,0.8,0.1)
-show(H)
+H₀ = LRA(sparse(H),0.2,0.4,0.1)
+show(typeof(H₀))
 
-#δH = 0.01*inv(H)
+δH = 0.01*inv(H₀)
+Hsum = H₀ + δH
+
+show(typeof(Hsum))
