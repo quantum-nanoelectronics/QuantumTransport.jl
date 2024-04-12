e979e2b0487477799b7a195c749a969e338c87a5
Vivian Rogers <vivian.rogers@utexas.edu>
2024-04-11 21:33:35 -0500

    Fixed the default runs.jl

--- user-input/ex_runs.jl
+++ user-input/ex_runs.jl
@@ -6,14 +6,14 @@ include("./ex_materials.jl")
 runparams = Dict(
 	     "path" => "./",
 		 #"material_hamiltonian" => material_hamiltonians,
-		 "material_params" => Dict(t = 1.0, ε₀ = 1.0),
+		 "material_params" => Dict(t => 1.0, ε₀ => 1.0),
 	     # so, for runs looking at the electronic properties of just one unit cell
-	     "unitcell" => Dict("material"="metal", "bands" => true, "bands_project" => [σ[1],γ⁵], "save"=>[:bandstructure, :DOS], "poisson" => false, "DOS" => false),
+	     "unitcell" => Dict("material"=> "metal", "bands" => true, "bands_project" => [σ[1],γ⁵], "save"=>[:bandstructure, :DOS], "poisson" => false, "DOS" => false),
 	     # and for runs looking at the voltage-dependent transport properties
 	     "transport" => Dict("geometry" => devicegeometry, "ΔV" => 0.01, "μ" => 0.1*eV, "T" => 300, "η" => 1E-4*eV, "save" => [:transmission, :conductance],
 			  "Gʳinv_method" => :RGF, "D_spin" => 0.01*eV, "D_momentum" => 0.5*eV, "kspace"=>false),
 	     # and for runs where we want to slap a bunch of unit cells together and get the scattering-corrected electronic properties
-	     "supercell" => Dict("geometry" => devicegeometry, "bands_project" = [σ[1],σ[2]], "poisson"=>true, "μ" => 0.1*eV, "T" => 300, "η" => 1E-4*eV, "save" => [:unfoldedbands], "density_project" => [I(2),[σ[1],σ[2],σ[3]]],
+	     "supercell" => Dict("geometry" => devicegeometry, "bands_project" => [σ[1],σ[2]], "poisson"=>true, "μ" => 0.1*eV, "T" => 300, "η" => 1E-4*eV, "save" => [:unfoldedbands], "density_project" => [I(2),[σ[1],σ[2],σ[3]]],
 			  "Gʳinv_method" => :RGF, "D_dephasing" => 0.1*eV, "D_spin" => 0.01*eV, "D_momentum" => 0.5*eV)
 	     )
 
