cf3d3719aa05019d8dd4bf044f28a23e82a22422
Vivian Rogers <vivian.rogers@utexas.edu>
2024-04-19 18:23:05 -0500

    Squashed one bug in constructHamiltonian.

--- data-input/runs.jl
+++ data-input/runs.jl
@@ -12,10 +12,10 @@ runparams = Dict(
 	"unitcell" => Dict("geometry" => devicegeometry, "material"=> "metal", "bands" => true, "bands_project" => [σ[1]], "save"=>[:bandstructure], "poisson" => false, "DOS" => false, "klist" => ["Γ","X₁","M","X₂","Γ","X₃"], "numInterpolations" => 64),
 	
 	# and for runs looking at the conductance of a large supercell
-	"transport" => Dict("geometry" => devicegeometry, "ΔV" => 0.01, "μ" => 0.1*eV, "T" => 300, "η" => 1E-4*eV, "save" => [:transmission, :conductance], "electrodeMagnetization" => false, "deviceMagnetization" => false, "Gʳinv_method" => :RGF, "D_spin" => 0.01*eV, "D_momentum" => 0.0005*eV, "kspace"=>false, "E_samples" => [E for E = -2.0:0.1:4.0], "electrodeMaterial" => "metal"),
+	"transport" => Dict("geometry" => devicegeometry, "ΔV" => 0.001, "μ" => 0.0*eV, "T" => 1, "η" => 1E-5*eV, "save" => [:transmission, :conductance], "electrodeMagnetization" => false, "deviceMagnetization" => false, "Gʳinv_method" => :RGF, "D_spin" => 0.000001*eV, "D_momentum" => 0.000005*eV, "kspace"=>false, "E_samples" => [E for E = -3.0:0.1:5.0], "electrodeMaterial" => "metal"),
 	
 	# and for runs where we want to slap a bunch of unit cells together and get the scattering-corrected electronic properties
-	"supercell" => Dict("geometry" => devicegeometry, "bands_project" => [σ[1],σ[2]], "poisson"=>true, "μ" => 0.1*eV, "T" => 300, "η" => 1E-4*eV, "save" => [:unfoldedbands], "density_project" => [I(2),[σ[1],σ[2],σ[3]]], "Gʳinv_method" => :RGF, "D_dephasing" => 0.001*eV, "D_spin" => 0.0001*eV, "D_momentum" => 0.001*eV)
+	"supercell" => Dict("geometry" => devicegeometry, "bands_project" => [σ[1],σ[2]], "poisson"=>false, "μ" => 0.0*eV, "T" => 300, "η" => 1E-4*eV, "save" => [:unfoldedbands], "density_project" => [I(2),[σ[1],σ[2],σ[3]]], "Gʳinv_method" => :RGF, "D_dephasing" => 0.001*eV, "D_spin" => 0.0001*eV, "D_momentum" => 0.001*eV)
 )
 
 # all parameters of the simulation must be passed in through the runparams, and now we will ensure they are added
