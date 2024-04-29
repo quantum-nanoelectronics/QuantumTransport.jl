5a8317f44f0f392028640c33815f1dae321a5bcb
somwakdikar <89665546+somwakdikar@users.noreply.github.com>
2024-04-29 11:28:43 -0500

    makie

--- test/visualization.jl
+++ test/visualization.jl
@@ -8,11 +8,12 @@ function runVisualizationTests()
     filename = "transmission.csv"  # specify the CSV file name
 
     file_path = joinpath(readDir, filename)
-    call_function_based_on_header(readDir, filename)
-    filename = "CNTpositions.csv"
-    call_function_based_on_header(readDir, filename)
+    fig = call_function_based_on_header(readDir, filename)
+    # filename = "CNTpositions.csv"
+    # call_function_based_on_header(readDir, filename)
     # Save the figure
     @test isfile(file_path)
+    return fig
 end
 
 function runVisualizationTestsGLMakie()
